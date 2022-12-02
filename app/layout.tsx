3f831dbbfa26d408c8a2f34597b6bf270f840037
Cory Hutchison <cory@mutecipher.com>
2022-12-02 11:31:56 -0700

    Add Inter and JetBrains_Mono fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,15 @@
 import '../styles/globals.css'
+import { Inter, JetBrains_Mono } from '@next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-jetbrains-mono',
+})
 
 export default function RootLayout({
   children,
@@ -6,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`antialiased ${inter.variable} ${jetBrainsMono.variable}`} >
       <body>{children}</body>
-    </html>
+    </html >
   )
 }
